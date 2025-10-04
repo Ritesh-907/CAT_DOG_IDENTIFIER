6cd3637f6ee0e74c49cb7cbe4597f22865702ae2
Ritesh Chaudhary <chaudharyritesh.7777@gmail.com>
2025-10-04 20:34:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ def upload():
     return jsonify({"prediction": result})
 
 if __name__ == "__main__":
-    # For Render: host must be 0.0.0.0
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
